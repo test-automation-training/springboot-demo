42b84bb60fe945b1e7edd110ce912c9684a5d9ba
hhu <hhu@thoughtworks.com>
2018-07-20 15:33:53 +0800

    renaming

--- src/test/java/name/huhao/springbootdemo/controller/DemoControllerTest.java
+++ src/test/java/name/huhao/springbootdemo/controller/DemoControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DemoControllerUnitTest {
+public class DemoControllerTest {
 
     private DemoController controller;
 
